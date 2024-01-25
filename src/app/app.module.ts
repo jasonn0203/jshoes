daab0d5417db45d827635995fe83f8ee0d470136
Jason Huynh <jasonn0203@gmail.com>
2024-01-25 20:45:48 +0700

    init project

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,16 @@
+import { UserModule } from './user/user.module';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { RootComponent } from './admin/layout/root/root.component';
+import { AdminModule } from './admin/admin.module';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  declarations: [AppComponent, RootComponent],
+  imports: [BrowserModule, AppRoutingModule, UserModule, AdminModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
